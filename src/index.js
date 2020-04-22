ba518467d094d5ba567ea70573b6fef6e211fe18
Yueying <wangyy395@gmail.com>
2020-04-22 16:23:26 -0400

    add delete botton to each task

--- src/index.js
+++ src/index.js
@@ -8,12 +8,29 @@ document.addEventListener("DOMContentLoaded", () => {
   const submitButton = document.getElementsByTagName('input')[1];
   const userInput = document.getElementsByTagName('input')[0];
 
-  submitButton.addEventListener('click', function(event){   
+  submitButton.addEventListener('click', function(event){  
+    event.preventDefault();  
     let task = userInput.value;
     let taskUl = document.getElementById('tasks');
     let newLi = document.createElement('li');
     newLi.innerHTML = `${task}`;
-    taskUl.appendChild(newLi);
-    event.preventDefault();  
+    let deleteButton = document.createElement('button');
+    deleteButton.innerHTML = 'DELETE';
+    newLi.appendChild(deleteButton);
+    taskUl.appendChild(newLi);     
   })
+
+  const deleteButtons = document.querySelectorAll('button');
+  deleteButtons.forEach(function(button) {
+    button.addEventListener('click', function(event){
+      let bottonTarget = event.target;
+      let taskToDelete = bottonTarget.parentNode;
+      debugger;
+      taskToDelete.remove();
+    })
+  })
+
+  
 });
+
+
