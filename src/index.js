73ebf0ea02078d88b306e56ff0a719d8bc3cabfb
Yueying <wangyy395@gmail.com>
2020-04-22 15:25:34 -0400

    create listener to submit button

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  //TODOS:
+  //1. locate to button and create listener to it
+  //2. grab user's input and append it to ul
+  //3. prevent the submit??  Event.preventDefault
+
+  const submitButton = document.getElementsByTagName('input')[1];
+  const userInput = document.getElementsByTagName('input')[0];
+
+  submitButton.addEventListener('click', function(event){
+    event.preventDefault;
+    console.log(event);
+    let task = userInput.value;
+    let taskUl = document.getElementsByTagName;
+    let newLi = document.createElement('li');
+    newLi.innerHTML = `${task}`;
+    taskUl.appendChild(newLi);
+  })
+
 });
