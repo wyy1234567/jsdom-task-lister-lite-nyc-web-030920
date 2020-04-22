7223ead12e95a72ac7dc5f8d279c2a3e12f27d7b
Yueying <wangyy395@gmail.com>
2020-04-22 17:20:34 -0400

    Todo: fix const deleteBottons

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
+
   // your code here
   //TODOS:
   //1. locate to button and create listener to it
   //2. grab user's input and append it to ul
   //3. prevent the submit??  Event.preventDefault
 
+// A priority value selected from a dropdown that is used to determine the color of the text in the list (e.g. red for high priority, yellow for medium, green for low)
+// As a challenge, implement a sorting functionality that displays the tasks ascending or descending order based on priority
+
   const submitButton = document.getElementsByTagName('input')[1];
   const userInput = document.getElementsByTagName('input')[0];
   const taskUl = document.getElementById('tasks');
+  
   
 
   submitButton.addEventListener('click', function(event){  
@@ -22,16 +26,13 @@ document.addEventListener("DOMContentLoaded", () => {
   })
 
 
-  const deleteButtons = document.querySelectorAll('button');
+  let deleteButtons = document.querySelectorAll('button');
   deleteButtons.forEach(function(button) {
     button.addEventListener('click', function(event){
-      debugger;
       const taskToDelete = event.target.parentNode;
       taskToDelete.remove();
     })
   })
 
-  
-});
 
 
